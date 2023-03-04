67a088f6d9de5a70d35c6f1f34c696fe1e06a564
Parthiv Sadhukhan <95235445+Parthiv-01@users.noreply.github.com>
2023-03-04 19:41:22 +0530

    update v.0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer","Codeamn","Quicklearner"],
+    strings: ["Frontend Developer","Coder","Quicklearner"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
